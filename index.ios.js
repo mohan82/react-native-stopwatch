4f9e12ae328daa38ee3119f1fa8e76ed8d22cbd2
Mohan Ambalavanan <mohanambalavanan@gmail.com>
2016-06-25 00:52:14 +1000

    Refactored application to its own folder

--- index.ios.js
+++ index.ios.js
@@ -5,31 +5,11 @@
  */
 
 import React, { Component } from 'react';
+import  StopWatchApplication from './src/StopWatchApplication'
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  AppRegistry
 } from 'react-native';
 
-class Stopwatch extends  React.Component {
-    render(){
-        return(<View style={styles.container}><Text style={styles.text}>Hello World</Text></View>)
-    }
-}
-let styles = StyleSheet.create({
-
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-        backgroundColor:'#29ABA4',
-    },
-    text:{
-        fontSize:20,
-        color:'white'
-    }
-});
 
 
-AppRegistry.registerComponent('stopwatch', ()=>Stopwatch);
+AppRegistry.registerComponent('stopwatch', ()=>StopWatchApplication);
